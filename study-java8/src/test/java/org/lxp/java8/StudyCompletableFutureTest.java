46782e563721be0eef57b5b38a1b36ef0af16706
Super.Li <xupenglee@gmail.com>
2017-12-12 21:32:59 +0800

    more tests for CompletableFuture

--- study-java8/src/test/java/org/lxp/java8/StudyCompletableFutureTest.java
+++ study-java8/src/test/java/org/lxp/java8/StudyCompletableFutureTest.java
@@ -1,6 +1,8 @@
 package org.lxp.java8;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Assert;
@@ -29,4 +31,45 @@ public class StudyCompletableFutureTest {
         Assert.assertEquals(StudyCompletableFuture.MAX_SIZE, pair.getRight().size());
     }
 
+    @Test
+    public void testSupplyAsyncAndThenApply() throws InterruptedException, ExecutionException {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 1;
+        }).thenApply(i -> i + 1);
+        Assert.assertEquals(2, future.get().intValue());
+    }
+
+    @Test
+    public void testSupplyAsyncAndThenCompose() throws InterruptedException, ExecutionException {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 1;
+        }).thenCompose(i -> CompletableFuture.supplyAsync(() -> i + 2));
+        Assert.assertEquals(3, future.get().intValue());
+    }
+
+    @Test
+    public void testSupplyAsyncAndAccept() throws InterruptedException, ExecutionException {
+        StringBuilder sb = new StringBuilder();
+        CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 1;
+        }).thenAccept(x -> {
+            sb.append(x + 3);
+        }).join();
+        Assert.assertEquals("4", sb.toString());
+    }
 }
