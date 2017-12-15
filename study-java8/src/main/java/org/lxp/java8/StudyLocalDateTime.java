206f2e006f8d6abc265fc35cae1150037cf3ccb7
Super.Li <xupenglee@gmail.com>
2017-12-15 02:48:39 -0600

    study LocalDateTime

---
+++ study-java8/src/main/java/org/lxp/java8/StudyLocalDateTime.java
@@ -0,0 +1,47 @@
+package org.lxp.java8;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+public class StudyLocalDateTime {
+    public static String get20170701ViaCalendar(String format) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2017, 6, 1, 0, 0, 0);
+        return new SimpleDateFormat(format).format(calendar.getTime());
+    }
+
+    public static String get20170701ViaLocalDateTime(String format) {
+        LocalDateTime localDateTime = LocalDateTime.of(2017, 7, 1, 0, 0);
+        return format(localDateTime, format);
+    }
+
+    public static String format(LocalDateTime localDateTime, String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return localDateTime.format(formatter);
+    }
+
+    public static long getMillis(LocalDateTime localDateTime) {
+        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    public static LocalDateTime parseViaLocalDateTime(String date, String format) {
+        return LocalDateTime.parse(date, DateTimeFormatter.ofPattern(format));
+    }
+
+    public static Date parseViaDateFromat(String date, String format) throws ParseException {
+        return new SimpleDateFormat(format).parse(date);
+    }
+
+    public static LocalDateTime addOneDay(LocalDateTime localDateTime) {
+        return localDateTime.plusDays(1);
+    }
+
+    public static LocalDateTime setDayOfMonth(LocalDateTime localDateTime, int dayOfMonth) {
+        return localDateTime.withDayOfMonth(dayOfMonth);
+    }
+}
---
+++ study-java8/src/test/java/org/lxp/java8/StudyLocalDateTimeTest.java
@@ -0,0 +1,47 @@
+package org.lxp.java8;
+
+import java.time.LocalDateTime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StudyLocalDateTimeTest {
+
+    @Test
+    public void testGet20170701() throws Exception {
+        String format = "yyyy-MM-dd HH:mm:ss";
+        Assert.assertEquals(StudyLocalDateTime.get20170701ViaCalendar(format),
+                StudyLocalDateTime.get20170701ViaLocalDateTime(format));
+        Assert.assertEquals("2017-07-01 00:00:00", StudyLocalDateTime.get20170701ViaLocalDateTime(format));
+    }
+
+    @Test
+    public void testGetMillisOfNow() throws Exception {
+        Assert.assertEquals(System.currentTimeMillis() / 1000,
+                StudyLocalDateTime.getMillis(LocalDateTime.now()) / 1000);
+    }
+
+    @Test
+    public void testParse() throws Exception {
+        String format = "yyyy-MM-dd HH:mm:ss";
+        String date = "2017-07-01 00:00:00";
+        Assert.assertEquals(StudyLocalDateTime.parseViaDateFromat(date, format).getTime(),
+                StudyLocalDateTime.getMillis(StudyLocalDateTime.parseViaLocalDateTime(date, format)));
+    }
+
+    @Test
+    public void testAddOneDay() throws Exception {
+        String format = "yyyy-MM-dd HH:mm:ss";
+        String date = "2017-07-31 00:00:00";
+        Assert.assertEquals("2017-08-01 00:00:00", StudyLocalDateTime
+                .format(StudyLocalDateTime.addOneDay(StudyLocalDateTime.parseViaLocalDateTime(date, format)), format));
+    }
+
+    @Test
+    public void testSetDayOfMonth() throws Exception {
+        String format = "yyyy-MM-dd HH:mm:ss";
+        String date = "2017-07-01 00:00:00";
+        Assert.assertEquals("2017-07-15 00:00:00", StudyLocalDateTime.format(
+                StudyLocalDateTime.setDayOfMonth(StudyLocalDateTime.parseViaLocalDateTime(date, format), 15), format));
+    }
+}
