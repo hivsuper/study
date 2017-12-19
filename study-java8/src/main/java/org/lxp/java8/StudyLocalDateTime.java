aad10447d516fbd60324b554436c65991fae91e2
Super.Li <xupenglee@gmail.com>
2017-12-19 05:24:42 -0600

    more about LocalDateTime/LocalDate

--- study-java8/src/main/java/org/lxp/java8/StudyLocalDateTime.java
+++ study-java8/src/main/java/org/lxp/java8/StudyLocalDateTime.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
@@ -38,5 +39,22 @@ public class StudyLocalDateTime {
 
     public static LocalDateTime setDayOfMonth(LocalDateTime localDateTime, int dayOfMonth) {
         return localDateTime.withDayOfMonth(dayOfMonth);
+    }
+
+    public static boolean after(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
+        return localDateTime1.isAfter(localDateTime2);
+    }
+
+    public static boolean afterViaCompareTo(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
+        return localDateTime1.compareTo(localDateTime2) > 0;
+    }
+
+    public static long differentSeconds(LocalDateTime localDateTime1, LocalDateTime localDateTime2,
+            ZoneOffset zoneOffset) {
+        return localDateTime1.toEpochSecond(zoneOffset) - localDateTime2.toEpochSecond(zoneOffset);
+    }
+
+    public static long differentDays(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
+        return localDateTime1.toLocalDate().toEpochDay() - localDateTime2.toLocalDate().toEpochDay();
     }
 }
--- study-java8/src/test/java/org/lxp/java8/StudyLocalDateTimeTest.java
+++ study-java8/src/test/java/org/lxp/java8/StudyLocalDateTimeTest.java
@@ -1,6 +1,7 @@
 package org.lxp.java8;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -33,8 +34,8 @@ public class StudyLocalDateTimeTest {
     public void testAddOneDay() throws Exception {
         String format = "yyyy-MM-dd HH:mm:ss";
         String date = "2017-07-31 00:00:00";
-        Assert.assertEquals("2017-08-01 00:00:00", StudyDateTimeFormatter.format(
-                StudyLocalDateTime.addOneDay(StudyLocalDateTime.parseViaLocalDateTime(date, format)), format));
+        Assert.assertEquals("2017-08-01 00:00:00", StudyDateTimeFormatter
+                .format(StudyLocalDateTime.addOneDay(StudyLocalDateTime.parseViaLocalDateTime(date, format)), format));
     }
 
     @Test
@@ -43,5 +44,34 @@ public class StudyLocalDateTimeTest {
         String date = "2017-07-01 00:00:00";
         Assert.assertEquals("2017-07-15 00:00:00", StudyDateTimeFormatter.format(
                 StudyLocalDateTime.setDayOfMonth(StudyLocalDateTime.parseViaLocalDateTime(date, format), 15), format));
+    }
+
+    @Test
+    public void testAfter() throws Exception {
+        LocalDateTime localDateTime = LocalDateTime.of(2017, 7, 1, 0, 0);
+        Assert.assertTrue(StudyLocalDateTime.after(localDateTime, localDateTime.minusSeconds(1)));
+    }
+
+    @Test
+    public void testAfterViaCompareTo() throws Exception {
+        LocalDateTime localDateTime = LocalDateTime.of(2017, 7, 1, 0, 0);
+        Assert.assertFalse(StudyLocalDateTime.afterViaCompareTo(localDateTime.minusSeconds(1), localDateTime));
+        Assert.assertTrue(StudyLocalDateTime.afterViaCompareTo(localDateTime.plusSeconds(1), localDateTime));
+        Assert.assertTrue(StudyLocalDateTime.afterViaCompareTo(localDateTime.plusNanos(1), localDateTime));
+    }
+
+    @Test
+    public void testDifferentSeconds() throws Exception {
+        LocalDateTime localDateTime = LocalDateTime.of(2017, 7, 1, 0, 0);
+        Assert.assertEquals(-1L, StudyLocalDateTime.differentSeconds(localDateTime.minusSeconds(1), localDateTime,
+                ZoneOffset.ofHours(6)));
+    }
+
+    @Test
+    public void testDifferentDays() throws Exception {
+        LocalDateTime localDateTime = LocalDateTime.of(2017, 6, 1, 0, 0);
+        Assert.assertEquals(-1L, StudyLocalDateTime.differentDays(localDateTime.minusSeconds(1), localDateTime));
+        Assert.assertEquals(0L, StudyLocalDateTime.differentDays(localDateTime.plusSeconds(1), localDateTime));
+        Assert.assertEquals(30L, StudyLocalDateTime.differentDays(localDateTime.plusMonths(1), localDateTime));
     }
 }
