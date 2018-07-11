260737b0fa3674187ec699f9be39428b9f7e4462
Super.Li <xupenglee@gmail.com>
2018-07-11 04:31:46 -0500

    more test about file upload

--- upload-test/src/main/java/com/upload/test/controller/TestController.java
+++ upload-test/src/main/java/com/upload/test/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.upload.test.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,8 +15,26 @@ import org.springframework.web.multipart.MultipartFile;
 public class TestController {
     @ResponseBody
     @PostMapping(value = "/fileAndUrl")
-    public void fileAndUrl(@RequestParam(value = "file", required = false) MultipartFile file,
-            @RequestParam(value = "url", required = false) String url) {
-        System.err.println("File: " + file + " and " + url);
+    public ResponseEntity<Void> fileAndUrl(@RequestParam(value = "file", required = false) MultipartFile file,
+            @RequestParam(value = "name", required = false) String name) throws Exception {
+        System.err.println("File: " + file + " and " + name);
+        file.transferTo(new File("D:/" + name));
+        return ResponseEntity.ok().build();
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/uploadMultipleFiles")
+    public ResponseEntity<Void> files(@RequestParam(value = "files", required = false) MultipartFile[] files)
+            throws Exception {
+        Arrays.stream(files).forEach(file -> {
+            try {
+                String fileName = file.getOriginalFilename();
+                System.err.println(fileName);
+                file.transferTo(new File("D:/" + fileName));
+            } catch (IllegalStateException | IOException e) {
+                e.printStackTrace();
+            }
+        });
+        return ResponseEntity.ok().build();
     }
 }
